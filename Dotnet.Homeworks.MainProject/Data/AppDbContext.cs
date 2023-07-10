33271dc8466652439b9e29e469de47932575472b
NikitaLopukhin <nikita.l.boss@gmail.com>
2023-07-10 17:07:44 +0300

    Создал пустой шаблон проекта интернет-магазина

---
+++ Dotnet.Homeworks.MainProject/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Dotnet.Homeworks.MainProject.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Dotnet.Homeworks.MainProject.Data;
+
+public class AppDbContext : DbContext
+{
+    public DbSet<User> Users { get; set; }
+    public DbSet<Product> Products { get; set; }
+    
+    public AppDbContext() { }
+
+    public AppDbContext(DbContextOptions options) : base(options) { }
+}
