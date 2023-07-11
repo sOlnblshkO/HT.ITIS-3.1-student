b949a2eda5fdddc80848e6ad9792a9016103c6b6
NikitaLopukhin <nikita.l.boss@gmail.com>
2023-07-11 11:35:13 +0300

    Убрал валидирование на уровне сущности

--- Dotnet.Homeworks.MainProject/Models/User.cs
+++ Dotnet.Homeworks.MainProject/Models/User.cs
@@ -1,10 +1,6 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace Dotnet.Homeworks.MainProject.Models;
+﻿namespace Dotnet.Homeworks.MainProject.Models;
 
 public class User : BaseEntity
 {
-    [Required]
-    [EmailAddress]
     public string Email { get; set; }
 }
