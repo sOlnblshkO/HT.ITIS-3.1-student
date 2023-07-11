b61a3ebe8858140dcdd37e8416f4d664cab0392a
selezen4ever <apdasd@mail.ru>
2023-07-11 17:39:20 +0300

    Покрыл тестами hw 2 (docker)

---
+++ Dotnet.Homeworks.Tests/Docker/DockerCompose.cs
@@ -0,0 +1,26 @@
+﻿namespace Dotnet.Homeworks.Tests.Docker;
+
+public class DockerCompose
+{
+    public ServicesSection? Services { get; set; }
+}
+
+public class ServicesSection
+{
+    public DotnetWeb? DotnetWeb { get; set; }
+    
+    public DotnetPostgres? DotnetPostgres { get; set; }
+}
+
+public class DotnetWeb
+{
+    public Dictionary<string, string>? Environment { get; set; }
+    
+    public Dictionary<string, string>? Build { get; set; }
+    
+    public List<string>? DependsOn { get; set; }
+}
+public class DotnetPostgres
+{
+    public Dictionary<string, string>? Environment { get; set; }
+}
