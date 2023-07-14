3dfcebc35075e65e5bcba8061f0c9635a9cec763
NikitaLopukhin <nikita.l.boss@gmail.com>
2023-07-14 10:23:05 +0300

    Добавил Mailing.API проект, создал endpoint для отправки email, обновил Readme.md

---
+++ Dotnet.Homeworks.Mailing.API/Configuration/EmailConfig.cs
@@ -0,0 +1,9 @@
+﻿namespace Dotnet.Homeworks.Mailing.API.Configuration;
+
+public class EmailConfig
+{
+    public string Email { get; set; }
+    public string Host { get; set; }
+    public int Port { get; set; }
+    public string Password { get; set; }
+}
