726089caf177a1a31a585a05b943b9dcdf4c8802
NikitaLopukhin <nikita.l.boss@gmail.com>
2023-07-20 14:12:38 +0300

    Добавил изначальную проверку на наличие каких-либо консьюмеров в сборке

---
+++ Dotnet.Homeworks.Tests/MasstransitRabbit/Helpers/NoConsumersInAssemblyException.cs
@@ -0,0 +1,12 @@
+﻿namespace Dotnet.Homeworks.Tests.MasstransitRabbit.Helpers;
+
+public class NoConsumersInAssemblyException : Exception
+{
+    public NoConsumersInAssemblyException(string assembly)
+        : base(FormatMessage(assembly)) { }
+
+    public NoConsumersInAssemblyException(string assembly, Exception innerException)
+        : base(FormatMessage(assembly), innerException) { }
+
+    private static string FormatMessage(string assembly) => $"There is no found consumers in {assembly} assembly";
+}
