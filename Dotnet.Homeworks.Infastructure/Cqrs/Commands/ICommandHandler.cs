2688520d3a670ed8c4a87b0c86884a2f7a1be344
RusLoneWorker <60140895+RusPyAloneWorker@users.noreply.github.com>
2023-07-25 12:40:45 +0300

    Update Dotnet.Homeworks.Infastructure/Cqrs/Commands/ICommandHandler.cs
    Co-authored-by: sOlnblshkO <k-mustafin@bk.ru>

--- Dotnet.Homeworks.Infastructure/Cqrs/Commands/ICommandHandler.cs
+++ Dotnet.Homeworks.Infastructure/Cqrs/Commands/ICommandHandler.cs
@@ -8,7 +8,7 @@ public interface ICommandHandler<TCommand> : IRequestHandler<TCommand, Result>
 {
 }
 
-public interface ICommandHandler<TCommand, TResponse> : IRequestHandler<TCommand, Result<TResponse>>
+public interface ICommandHandler<in TCommand, TResponse> : IRequestHandler<TCommand, Result<TResponse>>
     where TCommand : ICommand<TResponse>
 {
 }
