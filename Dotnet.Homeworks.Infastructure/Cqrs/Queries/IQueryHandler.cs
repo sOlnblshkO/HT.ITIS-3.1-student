2c4069035cbbf7bc6720c91f2c6311a4ef45d189
RusLoneWorker <60140895+RusPyAloneWorker@users.noreply.github.com>
2023-07-25 12:40:36 +0300

    Update Dotnet.Homeworks.Infastructure/Cqrs/Queries/IQueryHandler.cs
    Co-authored-by: sOlnblshkO <k-mustafin@bk.ru>

--- Dotnet.Homeworks.Infastructure/Cqrs/Queries/IQueryHandler.cs
+++ Dotnet.Homeworks.Infastructure/Cqrs/Queries/IQueryHandler.cs
@@ -4,7 +4,7 @@ using MediatR;
 namespace Dotnet.Homeworks.Infastructure.Cqrs.Queries;
 
 
-public interface IQueryHandler<TQuery, TResponse> : IRequestHandler<TQuery, Result<TResponse>>
+public interface IQueryHandler<in TQuery, TResponse> : IRequestHandler<TQuery, Result<TResponse>>
     where TQuery : IQuery<TResponse>
 {
     //public Task<Result<TResponse>> Handle(TQuery command, CancellationToken token);
