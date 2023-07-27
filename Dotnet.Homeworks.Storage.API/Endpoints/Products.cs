c233b075798eacae807f6bbb02f8f3bd54a6fd31
NikitaLopukhin <nikita.l.boss@gmail.com>
2023-07-27 11:27:25 +0300

    Убрал имплементацию эндпоинтов из Program.cs

---
+++ Dotnet.Homeworks.Storage.API/Endpoints/Products.cs
@@ -0,0 +1,46 @@
+﻿using Dotnet.Homeworks.Storage.API.Configuration;
+using Dotnet.Homeworks.Storage.API.Dto.Internal;
+using Dotnet.Homeworks.Storage.API.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dotnet.Homeworks.Storage.API.Endpoints;
+
+public static class Products
+{
+    public static void MapProductsEndpoints(this WebApplication app)
+    {
+        app.MapPost("/products/picture", async ([FromForm] IFormFile formFile, IStorageFactory storageFactory, CancellationToken cancellationToken) =>
+        {
+            var imageStorage = await storageFactory.CreateImageStorageWithinBucketAsync(Constants.ProductsBucket);
+            var image = new Image(formFile.OpenReadStream(), formFile.FileName,
+                !string.IsNullOrWhiteSpace(formFile.ContentType) ? formFile.ContentType : default);
+            var res = await imageStorage.PutItemAsync(image, cancellationToken);
+            return res.IsSucceeded ? Results.Ok() : Results.BadRequest(res.ResultMessage);
+        });
+
+        app.MapGet("/products/picture",
+            async (string fileName, IStorageFactory storageFactory, CancellationToken cancellationToken) =>
+            {
+                var imageStorage = await storageFactory.CreateImageStorageWithinBucketAsync(Constants.ProductsBucket);
+                var image = await imageStorage.GetItemAsync(fileName, cancellationToken);
+                return image.Content is null
+                    ? Results.NotFound()
+                    : Results.File(image.Content, image.ContentType, image.FileName);
+            });
+
+        app.MapDelete("/products/picture",
+            async (string fileName, IStorageFactory storageFactory, CancellationToken cancellationToken) =>
+            {
+                var imageStorage = await storageFactory.CreateImageStorageWithinBucketAsync(Constants.ProductsBucket);
+                var res = await imageStorage.RemoveItemAsync(fileName, cancellationToken);
+                return res.IsSucceeded ? Results.Ok() : Results.BadRequest(res.ResultMessage);
+            });
+
+        app.MapGet("/products/pictures", async (IStorageFactory storageFactory, CancellationToken cancellationToken) =>
+        {
+            var imageStorage = await storageFactory.CreateImageStorageWithinBucketAsync(Constants.ProductsBucket);
+            var res = await imageStorage.EnumerateItemNamesAsync(cancellationToken);
+            return Results.Ok(res);
+        });
+    }
+}
--- Dotnet.Homeworks.Storage.API/Program.cs
+++ Dotnet.Homeworks.Storage.API/Program.cs
@@ -1,7 +1,6 @@
 using Dotnet.Homeworks.Storage.API.Configuration;
-using Dotnet.Homeworks.Storage.API.Dto.Internal;
+using Dotnet.Homeworks.Storage.API.Endpoints;
 using Dotnet.Homeworks.Storage.API.Services;
-using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,42 +9,7 @@ builder.Services.AddSingleton<IStorageFactory, StorageFactory>();
 builder.Services.AddHostedService<PendingObjectsProcessor>();
 
 var app = builder.Build();
-#region Product endpoints
 
-app.MapPost("/products/picture", async ([FromForm] IFormFile formFile, IStorageFactory storageFactory, CancellationToken cancellationToken) =>
-{
-    var imageStorage = await storageFactory.CreateImageStorageWithinBucketAsync(Constants.ProductsBucket);
-    var image = new Image(formFile.OpenReadStream(), formFile.FileName,
-        !string.IsNullOrWhiteSpace(formFile.ContentType) ? formFile.ContentType : default);
-    var res = await imageStorage.PutItemAsync(image, cancellationToken);
-    return res.IsSucceeded ? Results.Ok() : Results.BadRequest(res.ResultMessage);
-});
-
-app.MapGet("/products/picture",
-    async (string fileName, IStorageFactory storageFactory, CancellationToken cancellationToken) =>
-    {
-        var imageStorage = await storageFactory.CreateImageStorageWithinBucketAsync(Constants.ProductsBucket);
-        var image = await imageStorage.GetItemAsync(fileName, cancellationToken);
-        return image.Content is null
-            ? Results.NotFound()
-            : Results.File(image.Content, image.ContentType, image.FileName);
-    });
-
-app.MapDelete("/products/picture",
-    async (string fileName, IStorageFactory storageFactory, CancellationToken cancellationToken) =>
-    {
-        var imageStorage = await storageFactory.CreateImageStorageWithinBucketAsync(Constants.ProductsBucket);
-        var res = await imageStorage.RemoveItemAsync(fileName, cancellationToken);
-        return res.IsSucceeded ? Results.Ok() : Results.BadRequest(res.ResultMessage);
-    });
-
-app.MapGet("/products/pictures", async (IStorageFactory storageFactory, CancellationToken cancellationToken) =>
-{
-    var imageStorage = await storageFactory.CreateImageStorageWithinBucketAsync(Constants.ProductsBucket);
-    var res = await imageStorage.EnumerateItemNamesAsync(cancellationToken);
-    return Results.Ok(res);
-});
-
-#endregion
+app.MapProductsEndpoints();
 
 app.Run();
