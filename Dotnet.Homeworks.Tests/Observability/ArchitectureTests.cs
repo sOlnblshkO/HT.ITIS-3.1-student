28c586ad5b5beac5cf91dd912b60dee2ef7450b1
NikitaLopukhin <nikita.l.boss@gmail.com>
2023-08-09 14:41:59 +0300

    Заменил Fact тесты на Theory

--- Dotnet.Homeworks.Tests/Observability/ArchitectureTests.cs
+++ Dotnet.Homeworks.Tests/Observability/ArchitectureTests.cs
@@ -9,35 +9,24 @@ namespace Dotnet.Homeworks.Tests.Observability;
 public class ArchitectureTests
 {
     private readonly Assembly _mainAssembly = AssemblyReference.Assembly;
-    
-    [Homework(RunLogic.Homeworks.RabbitMasstransit)]
-    public void MainProject_ShouldHave_DependencyOn_OpenTelemetry()
+
+    [HomeworkTheory(RunLogic.Homeworks.RabbitMasstransit)]
+    [InlineData(Constants.OpenTelemetry)]
+    [InlineData(Constants.OpenTelemetryTrace)]
+    [InlineData(Constants.OpenTelemetryMetrics)]
+    public void MainProject_ShouldHave_Dependencies(string dependency)
     {
-        var types = GetTypesInAssemblyThatHaveDependency(_mainAssembly, Constants.OpenTelemetry);
-        
-        Assert.NotEmpty(types);
-    }
-    
-    [Homework(RunLogic.Homeworks.RabbitMasstransit)]
-    public void MainProject_ShouldHave_DependencyOn_OpenTelemetryTrace()
-    {
-        var types = GetTypesInAssemblyThatHaveDependency(_mainAssembly, Constants.OpenTelemetryTrace);
-        
-        Assert.NotEmpty(types);
-    }
-    
-    [Homework(RunLogic.Homeworks.RabbitMasstransit)]
-    public void MainProject_ShouldHave_DependencyOn_OpenTelemetryMetrics()
-    {
-        var types = GetTypesInAssemblyThatHaveDependency(_mainAssembly, Constants.OpenTelemetryMetrics);
-        
+        var types = GetTypesInAssemblyThatHaveDependency(_mainAssembly, dependency);
+
         Assert.NotEmpty(types);
     }
 
-    private static IEnumerable<Type> GetTypesInAssemblyThatHaveDependency(Assembly assembly, string dependency) =>
-        Types
+    private static IEnumerable<Type> GetTypesInAssemblyThatHaveDependency(Assembly assembly, string dependency)
+    {
+        return Types
             .InAssembly(assembly)
             .That()
             .HaveDependencyOn(dependency)
             .GetTypes();
+    }
 }
