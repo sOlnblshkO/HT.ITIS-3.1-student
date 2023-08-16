29f9e3366155038392235275be5aec359f76ea28
ruspyaloneworker <DioBrando2003@yandex.ru>
2023-08-16 16:44:39 +0300

    Константы

--- Dotnet.Homeworks.Tests.RunLogic/Utils/Cqrs/Constants.cs
+++ Dotnet.Homeworks.Tests.RunLogic/Utils/Cqrs/Constants.cs
@@ -1,10 +1,12 @@
-﻿namespace Dotnet.Homeworks.Tests.RunLogic.Utils.Cqrs;
+﻿using Dotnet.Homeworks.Features.Cqrs.UserManagement;
+
+namespace Dotnet.Homeworks.Tests.RunLogic.Utils.Cqrs;
 
 public static class Constants
 {
     public const string MediatR = "MediatR";
-    public static string? MainProjectNamespace = typeof(MainProject.Helpers.AssemblyReference).Assembly.GetName().Name;
-    public static string? CustomMediatorNamespace = typeof(Mediator.Helpers.AssemblyReference).Assembly.GetName().Name;
-    public const string UsersFeatureNamespace = "Dotnet.Homeworks.Features.Cqrs.Users";
-    public const string UserManagementFeatureNamespace = "Dotnet.Homeworks.Features.Cqrs.UserManagement";
+    public static string? MainProjectNamespace = MainProject.Helpers.AssemblyReference.Assembly.GetName().Name;
+    public static string? CustomMediatorNamespace = Mediator.Helpers.AssemblyReference.Assembly.GetName().Name;
+    public static string UsersFeatureNamespace = Features.Cqrs.Users.DirectoryReference.Namespace;
+    public static string UserManagementFeatureNamespace = DirectoryReference.Namespace;
 }
