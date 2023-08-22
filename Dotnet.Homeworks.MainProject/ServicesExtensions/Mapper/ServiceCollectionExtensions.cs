b28b332f056ddc325bc4eac3eeabd99c1cd26280
scilightener <nikita.l.boss@gmail.com>
2023-08-22 12:06:42 +0300

    Исправил замечания

--- Dotnet.Homeworks.MainProject/ServicesExtensions/Mapper/ServiceCollectionExtensions.cs
+++ Dotnet.Homeworks.MainProject/ServicesExtensions/Mapper/ServiceCollectionExtensions.cs
@@ -2,10 +2,11 @@ using System.Reflection;
 
 namespace Dotnet.Homeworks.MainProject.ServicesExtensions.Mapper;
 
-public static class AddMappersExtension
+public static class ServiceCollectionExtensions
 {
     public static IServiceCollection AddMappers(this IServiceCollection services, Assembly mapperConfigsAssembly)
     {
+        // TODO: добавить автоматическую регистрацию всех мапперов, найденных в переданной сборке, с помощью рефлексии
         throw new NotImplementedException();
     }
 }
